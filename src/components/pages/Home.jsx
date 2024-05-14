0f3a22dcae582f71a1d94ba168e0348740dd49ad
Hamza <hamzachaudhay79@gmail.com>
2024-05-15 02:11:48 +0500

    Again comminting

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,7 +11,22 @@ import BlogSection from "../Section/BlogSection";
 import DepartmentSection from "../Section/DepartmentSection";
 import Spacing from "../Spacing";
 import DepartmentSectionStyle1 from "../WebChildEmergency/DepartmentSectionStyle2";
-import { aboutMiniSvg, about_img, banner_img50, ctaBgSvg, departmentIcon1Svg, departmentIcon2Svg, departmentIcon3Svg, departmentIcon4Svg, departmentIcon5Svg, departmentIcon6Svg, heroBgJpeg, post1Jpeg, post2Jpeg, post3Jpeg } from "../imagepath";
+import {
+  aboutMiniSvg,
+  about_img,
+  banner_img50,
+  ctaBgSvg,
+  departmentIcon1Svg,
+  departmentIcon2Svg,
+  departmentIcon3Svg,
+  departmentIcon4Svg,
+  departmentIcon5Svg,
+  departmentIcon6Svg,
+  heroBgJpeg,
+  post1Jpeg,
+  post2Jpeg,
+  post3Jpeg,
+} from "../imagepath";
 import Gallery from "./Gallery";
 import WebCourseContent from "../WebLandingPage/WebCourseContent/WebCourseContent";
 import SessionCard from "../SessionCard";
@@ -80,39 +95,39 @@ const departmentData = [
 
 const workingProcessData = [
   {
-    title: 'Book Appointment',
+    title: "Book Appointment",
     subTitle:
-      'You can book an appointment with us by <br />calling our office, filling out an online form, or <br />using our mobile app.',
-    iconUrl: '/images/home_2/wording_process_icon_1.svg',
-    number: '01',
+      "You can book an appointment with us by <br />calling our office, filling out an online form, or <br />using our mobile app.",
+    iconUrl: "/images/home_2/wording_process_icon_1.svg",
+    number: "01",
   },
   {
-    title: 'Visit Our Facility',
+    title: "Visit Our Facility",
     subTitle:
-      'On the day of your appointment, come to our <br />facility at the designated time. Our staff will greet <br />you and guide you through the check-in process.',
-    iconUrl: '/images/home_2/wording_process_icon_2.svg',
-    number: '02',
+      "On the day of your appointment, come to our <br />facility at the designated time. Our staff will greet <br />you and guide you through the check-in process.",
+    iconUrl: "/images/home_2/wording_process_icon_2.svg",
+    number: "02",
   },
   {
-    title: 'Meet with Our Healthcare <br />Professionals',
+    title: "Meet with Our Healthcare <br />Professionals",
     subTitle:
-      'You will meet with one of our healthcare <br />professionals who will conduct a thorough <br />examination and provide a diagnosis or <br />treatment plan.',
-    iconUrl: '/images/home_2/wording_process_icon_3.svg',
-    number: '03',
+      "You will meet with one of our healthcare <br />professionals who will conduct a thorough <br />examination and provide a diagnosis or <br />treatment plan.",
+    iconUrl: "/images/home_2/wording_process_icon_3.svg",
+    number: "03",
   },
   {
-    title: 'Follow-up Care',
+    title: "Follow-up Care",
     subTitle:
-      'We will schedule any necessary follow-up <br />appointments, tests, or procedures to ensure <br />that you receive the best possible care.',
-    iconUrl: '/images/home_2/wording_process_icon_4.svg',
-    number: '04',
+      "We will schedule any necessary follow-up <br />appointments, tests, or procedures to ensure <br />that you receive the best possible care.",
+    iconUrl: "/images/home_2/wording_process_icon_4.svg",
+    number: "04",
   },
   {
-    title: 'Insurance and Billing',
+    title: "Insurance and Billing",
     subTitle:
-      'We accept most major insurance plans and <br />our billing department will work with you to <br />ensure that you understand your coverage <br />and any out-of-pocket expenses.',
-    iconUrl: '/images/home_2/wording_process_icon_5.svg',
-    number: '05',
+      "We accept most major insurance plans and <br />our billing department will work with you to <br />ensure that you understand your coverage <br />and any out-of-pocket expenses.",
+    iconUrl: "/images/home_2/wording_process_icon_5.svg",
+    number: "05",
   },
 ];
 
@@ -120,88 +135,117 @@ export default function Home() {
   // const navigate = useNavigate();
 
   // Hero Section useQuery
-  const { data: heroData, isLoading: heroLoading, error: heroError } = useQuery({
+  const {
+    data: heroData,
+    isLoading: heroLoading,
+    error: heroError,
+  } = useQuery({
     queryKey: ["HeroSection"],
-    queryFn: () => getAllDocuments("HeroSection").then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        title: doc.data().heroTitle,
-        subTitle: doc.data().heroSubtitle,
-        imgUrl: doc.data().heroBackground,
-      }));
-      return data[0];
-    }),
+    queryFn: () =>
+      getAllDocuments("HeroSection").then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          title: doc.data().heroTitle,
+          subTitle: doc.data().heroSubtitle,
+          imgUrl: doc.data().heroBackground,
+        }));
+        return data[0];
+      }),
   });
 
   // Child Emergency Header useQuery
-  const { data: CEHeader, isLoading: CEHeaderLoading, error: CEHeaderError } = useQuery({
+  const {
+    data: CEHeader,
+    isLoading: CEHeaderLoading,
+    error: CEHeaderError,
+  } = useQuery({
     queryKey: ["ChildEmergencyHeader"],
-    queryFn: () => getAllDocuments("ChildEmergencyHeader").then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        title: doc.data().CEHeadTitle,
-      }));
-      return data[0];
-    }),
+    queryFn: () =>
+      getAllDocuments("ChildEmergencyHeader").then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          title: doc.data().CEHeadTitle,
+        }));
+        return data[0];
+      }),
   });
 
   // Child Emergency Body useQuery
-  const { data: CEBody, isLoading: CEBodyLoading, error: CEBodyError } = useQuery({
+  const {
+    data: CEBody,
+    isLoading: CEBodyLoading,
+    error: CEBodyError,
+  } = useQuery({
     queryKey: ["ChildEmergencyBody"],
-    queryFn: () => getAllDocuments("ChildEmergencyBody").then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        title: doc.data().CEBodyTitle,
-        subTitle: doc.data().CEBodySubtitle,
-        description: doc.data().CEBodyDescription,
-      }));
-      return data;
-    }),
+    queryFn: () =>
+      getAllDocuments("ChildEmergencyBody").then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          title: doc.data().CEBodyTitle,
+          subTitle: doc.data().CEBodySubtitle,
+          description: doc.data().CEBodyDescription,
+        }));
+        return data;
+      }),
   });
 
   // Course Content Heading useQuery
-  const { data: CCHeadData, isLoading: CCHeadLoading, error: CCHeadError } = useQuery({
+  const {
+    data: CCHeadData,
+    isLoading: CCHeadLoading,
+    error: CCHeadError,
+  } = useQuery({
     queryKey: ["CourseContentHeading"],
-    queryFn: () => getAllDocuments("CourseContentHeading").then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        CCHeadTitle: doc.data().CCHeadTitle,
-        CCHeadSubtitle: doc.data().CCHeadSubtitle,
-      }));
-      return data[0];
-    }),
+    queryFn: () =>
+      getAllDocuments("CourseContentHeading").then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          CCHeadTitle: doc.data().CCHeadTitle,
+          CCHeadSubtitle: doc.data().CCHeadSubtitle,
+        }));
+        return data[0];
+      }),
   });
 
-   // Course Content Body useQuery
+  // Course Content Body useQuery
   const [CCBodyDataKey, setCCBodyDataKey] = useState([]);
 
-  const { data: CCBodyData, isLoading: CCBodyLoading, error: CCBodyError } = useQuery({
+  const {
+    data: CCBodyData,
+    isLoading: CCBodyLoading,
+    error: CCBodyError,
+  } = useQuery({
     queryKey: ["CourseContentBodyKey:", ...CCBodyDataKey], // Concatenating with a string
-    queryFn: () => getAllDocuments("CourseContentHeading").then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        CCTitle: doc.data().CCTitle,
-        CCSubtitle: doc.data().CCSubtitle,
-        CCQuote: doc.data().CCQuote,
-        CCImage: doc.data().CCImage
-      }));
+    queryFn: () =>
+      getAllDocuments("CourseContentHeading").then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          CCTitle: doc.data().CCTitle,
+          CCSubtitle: doc.data().CCSubtitle,
+          CCQuote: doc.data().CCQuote,
+          CCImage: doc.data().CCImage,
+        }));
 
-      setCCBodyDataKey(data);
-      return data;
-    }),
+        setCCBodyDataKey(data);
+        return data;
+      }),
   });
 
   // Organization Matters Heading useQuery
   // const [OMHeadDataKey, setOMHeadDataKey] = useState();
 
-  const { data: OMHeadData, isLoading: OMHeadLoading, error: OMHeadError } = useQuery({
+  const {
+    data: OMHeadData,
+    isLoading: OMHeadLoading,
+    error: OMHeadError,
+  } = useQuery({
     queryKey: ["OrganizationMattersHeading: "],
-    queryFn: () => getAllDocuments("OrganizationMattersHeading").then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => ({
-        OMHeadTitle: doc.data().OMHeadTitle,
-        OMHeadDescription: doc.data().OMHeadDescription,
-      }));
-      // setOMHeadDataKey(data[0]);
-      // console.log(data[0])
-      return data[0];
-    }),
+    queryFn: () =>
+      getAllDocuments("OrganizationMattersHeading").then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          OMHeadTitle: doc.data().OMHeadTitle,
+          OMHeadDescription: doc.data().OMHeadDescription,
+        }));
+        // setOMHeadDataKey(data[0]);
+        // console.log(data[0])
+        return data[0];
+      }),
   });
-  
 
   pageTitle("Home");
 
@@ -219,8 +263,7 @@ export default function Home() {
 
   return (
     <>
-
-    {/* Hero Section */}
+      {/* Hero Section */}
       <Hero
         title={heroData.title}
         subTitle={heroData.subTitle}
@@ -228,7 +271,7 @@ export default function Home() {
         imgUrl={heroData.imgUrl}
       />
 
-{/* Child Emergency Section */}
+      {/* Child Emergency Section */}
       <Section topMd={200} topLg={150} topXl={110}>
         {CEHeader && (
           <DepartmentSectionStyle1
@@ -240,7 +283,7 @@ export default function Home() {
 
       <Spacing md="182" lg="150" />
 
-{/* Course Content Section */}
+      {/* Course Content Section */}
       <WebCourseContent
         sectionTitle={CCHeadData.CCHeadTitle}
         sectionTitleUp=""
@@ -249,11 +292,10 @@ export default function Home() {
         data={workingProcessData}
       />
 
-<Spacing md="182" lg="150" />
+      <Spacing md="182" lg="150" />
 
-
-{/* Organization Matters Section */}
-<Section
+      {/* Organization Matters Section */}
+      <Section
         topMd={185}
         topLg={140}
         topXl={100}
@@ -261,10 +303,13 @@ export default function Home() {
         bottomLg={140}
         bottomXl={100}
       >
-        <FeaturesSection  sectionTitle={OMHeadData.OMHeadTitle} sectionSubtile={OMHeadData.OMHeadDescription} />
+        <FeaturesSection
+          sectionTitle={OMHeadData.OMHeadTitle}
+          sectionSubtile={OMHeadData.OMHeadDescription}
+        />
       </Section>
 
-{/* Training session */}
+      {/* Training session */}
       <div className="container cs_hero cs_style_1">
         <SectionHeading title="Upcoming Training Sessions" center={true} />
         <Spacing md="72" lg="50" />
