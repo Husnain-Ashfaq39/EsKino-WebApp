7e810a1696889c9e868325f84c791d4a8b5ea552
Husnain Ashfaq <i222536@nu.edu.pk>
2024-05-19 01:42:58 +0500

    modal styling issue solved

--- src/components/Section/AppointmentSection/index.jsx
+++ src/components/Section/AppointmentSection/index.jsx
@@ -7,7 +7,7 @@ export default function AppointmentSection({
   sectionId
 }) {
   return (
-    <div className="cs_shape_wrap ">
+    <div className="cs_shape_wrap">
      
       <div className="container">
         <div className="row align-items-center cs_gap_y_40">
