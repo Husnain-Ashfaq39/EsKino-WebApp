37321815e705a702d3ef65f766684928ea78c770
Ali Hamza <hamzachaudhay79@gmail.com>
2024-06-07 17:18:24 +0500

    About image fixed for infinite size of screens

--- src/components/Section/BannerSection/index.jsx
+++ src/components/Section/BannerSection/index.jsx
@@ -6,13 +6,24 @@ export default function Banner({ bgUrl, imgUrl, title, subTitle }) {
     <div className="container">
       <div
         className="cs_banner cs_style_1 cs_bg_filed"
-        style={{ background: colors.primary, backgroundSize: 'cover' }}
+        style={{ 
+          background: colors.primary, 
+          backgroundSize: 'cover',
+        }}
       >
-      <img src={imgUrl} alt="Banner" className="cs_banner_img rounded-xl" />
-
-
+        <img 
+          src={imgUrl} 
+          alt="Banner" 
+          className="cs_banner_img rounded-xl"
+          style={{
+            maxWidth: '100%',
+            // width: '100%',
+            height: 'auto',
+            maxHeight: '45rem' 
+          }}
+        />
         <h2 className="cs_banner_title cs_heading_color cs_fs_72">{title}</h2>
-        <p className="cs_banner_subtitle cs_heading_color cs_fs_20 cs_medium m-0" style={{color:colors.dark}}>
+        <p className="cs_banner_subtitle cs_heading_color cs_fs_20 cs_medium m-0" style={{color: colors.dark}}>
           {subTitle}
         </p>
       </div>
