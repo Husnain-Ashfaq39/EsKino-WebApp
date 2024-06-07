da8b346b4ddc81d47c9e06449b53ecef51ae7792
Ali Hamza <hamzachaudhay79@gmail.com>
2024-06-07 17:05:17 +0500

    Docotor page alignment

--- src/components/DoctorsComponent/Doctors.jsx
+++ src/components/DoctorsComponent/Doctors.jsx
@@ -3,6 +3,7 @@ import { susanne, Stephan } from '../imagepath';
 import styles from './Doctors.module.scss';
 import SectionHeading from '../SectionHeading';
 import Spacing from '../Spacing';
+
 const doctors = [
   {
     img: susanne,
@@ -43,44 +44,46 @@ const doctors = [
 
 const Doctors = () => {
   return (
-<>
-    <div className={styles.doctors}>
-      <div className={styles.container}>
-      <SectionHeading title='Über Uns' subTitle='Geleitet werden die Seminare von Susanne Nusser und Stephan Schwarz, beide Fachärzte für Kinder- und Jugendmedizin mit mehrjähriger Klinikerfahrung, die aber beide auch die Elternperspektive gut kennen.'/>
-      <Spacing md="72" lg="50" />
+    <>
+      <div className={styles.doctors}>
+        <div className={styles.container}>
+          <SectionHeading 
+            title='Über Uns' 
+            subTitle='Geleitet werden die Seminare von Susanne Nusser und Stephan Schwarz, beide Fachärzte für Kinder- und Jugendmedizin mit mehrjähriger Klinikerfahrung, die aber beide auch die Elternperspektive gut kennen.'
+          />
+          <Spacing md="72" lg="50" />
 
-        <div className={styles.wrapper}>
-          {doctors.map((doctor, index) => (
-            <div className={styles.card} key={index}>
-              <img src={doctor.img} alt={doctor.name} />
-              <div className={styles.info}>
-                <h3 className={styles.cardTitle}>{doctor.name}</h3>
-                <h5 className={styles.speciality}>{doctor.speciality}</h5>
-                <ul className={styles.detailsList}>
-                  {doctor.details.map((detail, i) => (
-                    <li key={i}>{detail}</li>
-                  ))}
-                  <br></br>
-                  <h5>Zusatzqualifikationen</h5>
-                  {doctor.additionalQualifications.map((qualification, i) => (
-                    <li key={i}>{qualification}</li>
-                  ))}
-                </ul>
-                <ul className={styles.currentPositions}>
-                  <h5>Aktuell</h5>
-                  {doctor.currentPositions.map((position, i) => (
-                    <li key={i}>{position}</li>
-                  ))}
-                </ul>
+          <div className={styles.wrapper}>
+            {doctors.map((doctor, index) => (
+              <div className={styles.card} key={index}>
+                <img src={doctor.img} alt={doctor.name} />
+                <div className={styles.info}>
+                  <h3 className={styles.cardTitle}>{doctor.name}</h3>
+                  <h5 className={styles.speciality}>{doctor.speciality}</h5>
+                  <ul className={styles.detailsList} style={{ textAlign: 'left'}} >
+                    {doctor.details.map((detail, i) => (
+                      <li key={i}>{detail}</li>
+                    ))}
+                    <br />
+                    <h5>Zusatzqualifikationen</h5>
+                    {doctor.additionalQualifications.map((qualification, i) => (
+                      <li style={{ textAlign: 'left', listStyleType: 'disc',marginLeft:"2.3rem" }} key={i}>{qualification}</li>
+                    ))}
+                  </ul>
+                  <ul className={styles.currentPositions} style={{ textAlign: 'left'}}>
+                    <h5>Aktuell</h5>
+                    {doctor.currentPositions.map((position, i) => (
+                      <li style={{ textAlign: 'left', listStyleType: 'disc',marginLeft:"2.3rem" }} key={i}>{position}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
-
 
 export default Doctors;
